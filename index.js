a9ab0c8114addca069191094b0b67a468b958e30
Tristan LaRoche <laroche11235@gmail.com>
2021-02-19 08:31:45 -0500

    add manager questions

--- index.js
+++ index.js
@@ -1,2 +1,59 @@
 const inquirer = require("inquirer");
 const fs = require('fs');
+
+
+const managerQuestions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            message: "What is your full name",
+            name: 'fullName',
+            validate: (answer) => {
+                if (answer.length < 1) {
+                    return console.log("A valid GitHub username is required.");
+                }
+                return true;
+            }
+        },
+        {
+            type: 'input',
+            message: "What is your employee ID",
+            name: 'employeeID',
+            validate: (answer) => {
+                if (answer.length < 1) {
+                    return console.log("A valid GitHub username is required.");
+                }
+                return true;
+            }
+        },
+        {
+            type: 'input',
+            message: "What is your email address",
+            name: 'email',
+            validate: (answer) => {
+                if (answer.length < 1) {
+                    return console.log("A valid GitHub username is required.");
+                }
+                return true;
+            }
+        },
+        {
+            type: 'input',
+            message: "What is office number",
+            name: 'officeNum',
+            validate: (answer) => {
+                if (answer.length < 1) {
+                    return console.log("A valid GitHub username is required.");
+                }
+                return true;
+            }
+        },
+        {
+            type: 'list',
+            message: "Choose the position of your team",
+            choices: ['Engineer', 'Intern', 'Done'],
+            name: 'license'
+        }
+    ])
+};
+
