7425e8cf0cff5cdf202c1c92b5fdbaf348044c65
Tristan LaRoche <laroche11235@gmail.com>
2021-02-25 16:20:11 -0500

    made base layout of classes

---
+++ classes.js
@@ -0,0 +1,28 @@
+class Employee {
+    constructor(name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email
+    }
+}
+
+class Manager extends Employee {
+    constructor(name, id, email, officeNum) {
+        super(name, id, email);
+        this.officeNum = officeNum;
+    }
+}
+
+class Engineer extends Employee {
+    constructor(name, id, email, githubAcc) {
+        super(name, id, email);
+        this.githubAcc = githubAcc;
+    }
+}
+
+class Intern extends Employee {
+    constructor(name, id, email, school) {
+        super(name, id, email);
+        this.school = school;
+    }
+}
