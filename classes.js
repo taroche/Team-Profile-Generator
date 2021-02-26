8025f251df90e1b0a5c1b904387b923e30de273e
Tristan LaRoche <laroche11235@gmail.com>
2021-02-26 13:12:35 -0500

    made get method for my classes

--- classes.js
+++ classes.js
@@ -5,12 +5,30 @@ class Employee {
         this.email = email
         this.role = role
     }
+    getName() {
+        return this.name;
+    }
+
+    getId() {
+        return this.id;
+    }
+
+    getEmail() {
+        return this.email;
+    }
+
+    getRole() {
+        return this.role;
+    }
 }
 
 class Manager extends Employee {
     constructor(name, id, email, role, officeNum) {
         super(name, id, email, role);
         this.officeNum = officeNum;
+    }
+    getOfficeNumber() {
+        return this.officeNumber;
     }
 }
 
@@ -19,12 +37,18 @@ class Engineer extends Employee {
         super(name, id, email);
         this.githubAcc = githubAcc;
     }
+    getGithub() {
+        return this.github;
+    }
 }
 
 class Intern extends Employee {
     constructor(name, id, email, role, school) {
         super(name, id, email, role);
         this.school = school;
+    }
+    getSchool() {
+        return this.school;
     }
 }
 
